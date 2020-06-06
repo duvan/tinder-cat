a5a033ffc879b1d5127b7a6ff687977de83223b2
duvanbelo@gmail.com <swordfish147/>
2020-06-06 11:54:02 -0500

    update login access to the server

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,59 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import { Profile } from './Profile'
 import { ViewCats } from './ViewCats'
 import { AboutCat } from './AboutCat'
 import { ProfileContextStore } from '../../contexts/ProfileContext';
 import { AboutContextStore }  from '../../contexts/AboutContext';
+import { HTTP_CONSTANTS } from '../../config/http-constants';
+import { requestHttp } from '../../config/http-server';
 
 export const Home = () => {
-
     
+    const [cat, setCat] = useState({})
+
+    const autologin = async () => {
+        try {
+            const endpoint = HTTP_CONSTANTS.autologin
+            const response = await requestHttp('post', endpoint)
+
+            const {status, cat } = response
+            if (response.status === 1) {
+                setCat(cat)
+            } else {
+                unauthorized()
+            }
+        } catch (err) {
+            unauthorized()
+        }
+    }
+
+    const unauthorized = () => {
+        sessionStorage.removeItem('_TOKEN_')
+        window.location.href = '/login'
+    }
+
+    useEffect  (() => {
+        autologin ()        
+
+        return () =>{}
+    }, [])
 
     return (
     <div className="home-page">
         <ProfileContextStore>
         <AboutContextStore>
-            <Profile />
-            <ViewCats />
-            <AboutCat />
+            { 
+                !Object.keys(cat).length //false == 0
+                ? <p>Loading app...</p>
+                : <Fragment>
+                    <Profile bio={cat.bio} nick={cat.nick} image={cat.image} />
+                    <ViewCats />
+                    <AboutCat />
+                </Fragment>
+            }   
         </AboutContextStore>
         </ProfileContextStore>
-        
+
     </div>
     )
 }
